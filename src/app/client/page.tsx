1508ac0f851aae7edd76a8376be19c87fec21adc
Harenabs21 <hei.harena.6@gmail.com>
2024-03-21 22:39:18 +0300

    fix the id and add priority constraint in image component

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -41,13 +41,13 @@ const PokemonList = () => {
       <h1 className="text-center text-3xl font-bold">Pokemon List</h1>
       <div className=" flex flex-wrap gap-3 items-center text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left">
       {pokemon.map((poke, index) => {
-        const id = poke.url.split('/')[6]; 
+        const id = poke.url.split('/')[6];
         return (
           <div
             className="group flex gap-3 flex-col items-center rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             key={index}
           >
-            <Image src={poke.image} alt={poke.name} width={100} height={100} />
+            <Image src={poke.image} alt={poke.name} width={100} height={100} priority />
             <p>{poke.name}</p>
             <div>
               <button onClick={() =>{router.push(`client/${id}`)}} className='rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500'> More Details</button>
